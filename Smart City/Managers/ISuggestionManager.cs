7865ff2b660f685185d3e5f4fc3d4dd4bc482d34
salma hani <salmahani0127@gmail.com>
2025-11-30 21:30:34 +0200

    Update ISuggestionManager.cs

--- Smart City/Managers/ISuggestionManager.cs
+++ Smart City/Managers/ISuggestionManager.cs
@@ -7,7 +7,7 @@ namespace Smart_City.Managers
         List<SuggestionDto> GetAll();
         SuggestionDto GetById(int id);
         List<SuggestionDto> GetByCitizenId(int citizenId);
-        SuggestionDto Create(SuggestionCreateDto dto);
+        SuggestionDto Create(SuggestionCreateDto dto, int citizenId);
         SuggestionDto Update(int id, SuggestionUpdateDto dto);
         bool Delete(int id);
     }
